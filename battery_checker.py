19109fabaf9170e4a74b297424c3ee458645453f
BoredBeing <134740689+BoredBeing@users.noreply.github.com>
2024-03-29 02:35:09 +0100

    Update battery_checker.py
    added comments

--- battery_checker.py
+++ battery_checker.py
@@ -3,12 +3,13 @@ import os
 import psutil
 import ctypes
 
-
-ctypes.windll.user32.MessageBoxW(0,u"Close this window now",u"Programm started",16)
+#Creates a starting conformation window
+ctypes.windll.user32.MessageBoxW(0,u"Close this window now",u"Programm started",16) 
 timer = 0
 battery = psutil.sensors_battery()
 shown = False
-def timeradd(t):
+
+def timeradd(t): 
     global timer
     timer += t
     time.sleep(t)
@@ -21,7 +22,8 @@ def main():
     while battery.power_plugged == False:
         if battery.percent < 60 and shown == False:
             timerPrint = timer/60
-            ctypes.windll.user32.MessageBoxW(0, u"Change battery now \n After "+str(timerPrint)+ "minutes of working 60 Percent has been reached.", u"Important Message",16)
+            #If you want to display something else edit or display it in here or create a additional window
+            ctypes.windll.user32.MessageBoxW(0, u"Change battery now \n After "+str(timerPrint)+ "minutes of working 60 Percent has been reached.", u"Important Message",16)  
             shown = True
             break
         
